314a41f229e16412f509d505861b86144a41b4a4
Ryan Altimari <ryanaltimari8@gmail.com>
2020-08-22 15:36:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(array)
+  counter = 0
+  answer = []
+  while counter < array.length
+    answer.push("Hello, my name is #{array[counter]}.")
+    counter = counter + 1 
+   end
+  return answer
+end
+
+
+
+def assign_rooms(name)
+  room_num = ["1", "2", "3", "4", "5", "6", "7"]
+  counter = 0
+  rooms = []
+  while counter < name.length
+  rooms.push("Hello, #{name[counter]}! You'll be assigned to room #{room_num[counter]}!") 
+  counter = counter + 1 
+  
+  end 
+  return rooms
+end
+
+
+def printer(assign_rooms)
+  
+end
+
+
+
+
+
+
